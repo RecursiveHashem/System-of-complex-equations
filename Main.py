421d770d223cd85cc4ff08b184fe493c2794a05d
RecursiveHashem <hashem.soliman05@gmail.com>
2026-03-12 22:49:48 +0200

    Logic errors

--- Main.py
+++ Main.py
@@ -15,52 +15,65 @@ class Complex:
 		else:
 			return "( "+ str(self.real)+ " + " + "j"+str(self.imag)+" )"
 	
-	
+	def __eq__(self, other):
+		if isinstance(other, Complex):
+			return self.real == other.real and self.imag == other.imag
+		return False
+
+	def __ne__(self, other):
+		return not self == other
+
 	def __add__(self,other):
-		if isinstance(other,Complex):
-			self.real+=other.real
-			self.imag+=other.imag
+		if isinstance(other, Complex):
+			r = self.real+other.real
+			i = self.imag+other.imag
 		else:
-			self.real+=other
-		return self
+			r = self.real+other
+			i = 0
+		return Complex(r, i)
 	
-	def __sub__(self,other):
-		if isinstance(other,Complex):
-			self.real-=other.real
-			self.imag-=other.imag
+	def __sub__(self, other):
+		if isinstance(other, Complex):
+			r = self.real-other.real
+			i = self.imag-other.imag
 		else:
-			self.real-=other
-		return self
+			r = self.real-other
+			i = 0
+		return Complex(r, i)
 	
-	def __mul__(self,other):
-		if isinstance(other,Complex):
+	def __mul__(self, other):
+		if isinstance(other, Complex):
 			r = self.real*other.real - (self.imag*other.imag)
 			i = self.real*other.imag + (self.imag*other.real)
-			self.real = r
-			self.imag = i
 		else:
-			self.real*=other
-			self.imag*=other
-		return self
+			r = self.real*other
+			i = self.imag*other
+		return Complex(r, i)
 	
 	def __truediv__(self, other):
 		if isinstance(other, Complex):
 			r = other.real/(other.real**2 + other.imag**2)
-			i =-other.imag/(other.real**2 + other.imag**2)
-			self*= Complex(r, i)
+			i = -other.imag/(other.real**2 + other.imag**2)
+			res = self * Complex(r, i)
 		else:
-			self.real /= other
-			self.imag /= other
-		return self
-		
+			res = Complex(self.real / other, self.imag / other)
+		return res
+
+	def __pow__(self, other):
+		res = Complex(1, 0)
+		i = -1 if other >= 0 else 1
+		while other:
+			res = self * res
+			other += i
+		return res
 	def abs(self):
 		return math.sqrt(self.real**2 + self.imag**2)
 	
 	def quad(self):
-		if self.real>=0:
-			return 1 if self.imag>=0 else 4
+		if self.real >= 0:
+			return 1 if self.imag >= 0 else 4
 		else:
-			return 2 if self.imag>=0 else 3
+			return 2 if self.imag >= 0 else 3
 	
 	def angle(self):
 		ang = math.degrees(math.atan(self.imag/self.real))
@@ -79,51 +92,67 @@ class Complex:
 
 class expression:
 	
-	def __init__(self, name):
+	def __init__(self, name, c, k):
 		self.varName = name
-		self.coef = Complex(1,0)
-		self.const = Complex(0,0)
+		self.coef = c
+		self.const = k
 	
 	def __str__(self):
-		return str(self.coef) + str(self.varName) + " + " + str(self.const)
+		if self.coef != Complex(0, 0) or str(self.coef) != "{0}":
+			return "[" + str(self.coef) + "]" + str(self.varName) + " + {" + str(self.const) + "}"
+		return "{" + str(self.const) + "}"
+
+	def __eq__(self, other):
+		if isinstance(other, expression):
+			return self.coef == other.coef and self.const == other.const and self.varName == self.varName
+		return False
+	def __ne__(self, other):
+		return not self == other
 	
 	def __add__(self, other):
 		if isinstance(other, expression):
 			pass
 		else:
-			self.const+=other
-		return self
+			res = expression(self.varName, self.coef, self.const + other)
+		return res
 		
 		
 	def __sub__(self, other):
 		if isinstance(other, expression):
 			pass
 		else:
-			self.const -= other
-		return self
+			res = expression(self.varName, self.coef, self.const - other)
+		return res
 	
 
 	def __mul__(self, other):
 		if isinstance(other, expression):
 			pass
 		else:
-			self.const *= other
-			self.coef *= other
-		return self
+			res = expression(self.varName, self.coef * other, self.const * other)
+		return res
 	
 	def __truediv__(self, other):
 		if isinstance(other, expression):
 			pass
 		else:
-			self.const /= other
-			self.coef /= other
-		return self
+			res = expression(self.varName, self.coef / other, self.const / other)
+		return res
+
+	def __pow__(self, other):
+		res = expression(self.varName, 0, 1)
+		i = -1 if other >= 0 else 1
+		while other:
+			res = self * res
+			other += i
+		return res
 
 
-# z = Complex(2,1)
-# x = Complex(0,2)
+z = Complex(0, 1)
+x = Complex(1, 0)
 
-v = expression("y")
+v = expression("y", x, z)
 
-print((v+2)/2)
+print(z**1)
+print(str(v))
 
